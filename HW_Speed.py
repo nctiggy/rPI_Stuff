7bfdcb878cb41aaf2bea480a0975db6f4d4b0d3d
Craig J Smith <nctiggy@gmail.com>
2014-04-28 17:39:50 -0700

    Change ext to py

---
+++ HW_Speed.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Example for RC timing reading for Raspberry Pi
+# Must be used with GPIO 0.3.1a or later - earlier verions
+# are not fast enough!
+
+import time
+import RPi.GPIO as GPIO, time, os
+import numpy
+
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+
+def RCtime (RCpin):
+        reading = 0
+        GPIO.setup(RCpin, GPIO.OUT)
+        GPIO.output(RCpin, GPIO.LOW)
+        time.sleep(0.005)
+
+        GPIO.setup(RCpin, GPIO.IN)
+        # This takes about 1 millisecond per loop cycle
+        while (GPIO.input(RCpin) == GPIO.LOW):
+                reading += 1
+        return reading
+
+calibrateA=[]
+calibrateB=[]
+for x in range(0, 20):
+	calibrateA.append(RCtime(17))
+triggerA = numpy.mean(calibrateA) * 1.75
+# print triggerA                               
+for x in range(0, 20):
+	calibrateB.append(RCtime(18))
+triggerB = numpy.mean(calibrateB) * 1.75
+#print triggerB
+
+while True:
+	while RCtime(17) < triggerA:
+        pass
+	#print time.asctime( time.localtime(time.time()) )
+	timeA = int(round(time.time() * 1000))
+	while RCtime(18) < triggerB:
+        pass
+	#print time.asctime( time.localtime(time.time()) )
+	timeB = int(round(time.time() *1000))
+	totalTime =  timeB-timeA
+	speedMPH = (3600000/totalTime)/4295.59
+	print str(format(speedMPH, '.2f')) + "MPH"
