f24fbf850df33d1d1f743b73262941b172017ee2
Craig J Smith <nctiggy@gmail.com>
2014-05-09 13:44:51 -0700

    Indent fix for reals
    textmate ;(

--- HW_Speed.py
+++ HW_Speed.py
@@ -17,39 +17,39 @@ headers = {'Content-type': 'application/json'}
 
 
 def RCtime (RCpin):
-        reading = 0
-        GPIO.setup(RCpin, GPIO.OUT)
-        GPIO.output(RCpin, GPIO.LOW)
-        time.sleep(0.005)
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.005)
 
-        GPIO.setup(RCpin, GPIO.IN)
-        # This takes about 1 millisecond per loop cycle
-        while (GPIO.input(RCpin) == GPIO.LOW):
-                reading += 1
-        return reading
+    GPIO.setup(RCpin, GPIO.IN)
+    # This takes about 1 millisecond per loop cycle
+    while (GPIO.input(RCpin) == GPIO.LOW):
+        reading += 1
+    return reading
 
 calibrateA=[]
 calibrateB=[]
 for x in range(0, 20):
-	calibrateA.append(RCtime(17))
+    calibrateA.append(RCtime(17))
 triggerA = numpy.mean(calibrateA) * 1.75
 # print triggerA                               
 for x in range(0, 20):
-	calibrateB.append(RCtime(18))
+    calibrateB.append(RCtime(18))
 triggerB = numpy.mean(calibrateB) * 1.75
 #print triggerB
 
 while True:
-	while RCtime(17) < triggerA:
-            pass
-	#print time.asctime( time.localtime(time.time()) )
-	timeA = int(round(time.time() * 1000))
-	while RCtime(18) < triggerB:
-            pass
-	#print time.asctime( time.localtime(time.time()) )
-	timeB = int(round(time.time() *1000))
-	totalTime =  timeB-timeA
-	speedMPH = (3600000/totalTime)/4295.59
+    while RCtime(17) < triggerA:
+        pass
+    #print time.asctime( time.localtime(time.time()) )
+    timeA = int(round(time.time() * 1000))
+    while RCtime(18) < triggerB:
+        pass
+    #print time.asctime( time.localtime(time.time()) )
+    timeB = int(round(time.time() *1000))
+    totalTime =  timeB-timeA
+    speedMPH = (3600000/totalTime)/4295.59
     data = {"auth_token":"YOUR_AUTH_TOKEN","current":speedMPH}
     data_json = json.dumps(data)
-	response = requests.post(url, data=data_json, headers=headers)
+    response = requests.post(url, data=data_json, headers=headers)
